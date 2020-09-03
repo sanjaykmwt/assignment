e360418267f8361665be1d3118fb75e1f7061323
sanju <sanjaykmtkmr@gmail.com>
2020-09-03 22:32:07 +0530

    basic code

--- server/dist/controllers/Controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Controller {
-    constructor() {
-        // this.session    =   ;
-    }
-}
-exports.default = Controller;
-//# sourceMappingURL=Controller.js.map
